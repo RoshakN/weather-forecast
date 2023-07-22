52575d35506ab2e7660e32b9001ed911450b8cd0
RoshakN <msnooraniz@gmail.com>
2023-07-22 09:27:12 +0330

    Fix center details component text

--- src/components/DetailComponent.jsx
+++ src/components/DetailComponent.jsx
@@ -3,7 +3,7 @@ import SunLineIcon from "remixicon-react/SunLineIcon";
 function DetailComponent(props) {
   return (
     <div className="flex flex-col items-center justify-center my-2 ">
-      <h1 className="font-semibold">{props.det}</h1>
+      <h1 className="font-semibold text-base text-center">{props.name}</h1>
       <SunLineIcon className="my-1" />
       <p>{props.number}</p>
     </div>
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -3,13 +3,13 @@ import DetailComponent from "./DetailComponent";
 function Details() {
   return (
     <div className="flex flex-row flex-wrap items-center justify-center w-4/5 mb-6">
-      <div className="flex flex-row flex-wrap justify-around w-1/2">
-        <DetailComponent det="Humidity" number="10%" />
-        <DetailComponent det="Chance of Rain" number="20%" />
+      <div className="flex flex-row flex-wrap justify-around items-center w-1/2">
+        <DetailComponent name="Humidity" number="10%" />
+        <DetailComponent name="Chance of Rain" number="20%" />
       </div>
-      <div className="flex flex-row flex-wrap justify-around w-1/2">
-        <DetailComponent det="Visibilty" number="30%" />
-        <DetailComponent det="UV Index" number="40%" />
+      <div className="flex flex-row flex-wrap justify-around items-center w-1/2">
+        <DetailComponent name="Visibilty" number="30%" />
+        <DetailComponent name="UV Index" number="40%" />
       </div>
     </div>
   );
