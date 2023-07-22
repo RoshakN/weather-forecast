2e71bd8e6362002533779fb69e34a493583d4b94
RoshakN <msnooraniz@gmail.com>
2023-07-22 11:25:50 +0330

    Fix UI touch box day components

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,26 +1,35 @@
 import SunLineIcon from "remixicon-react/SunLineIcon";
+import {
+  SunriseAlt,
+  Sunset,
+  TemperatureUp,
+  TemperatureDown,
+} from "react-flaticons";
 
 function Day(props) {
   return (
-    <div className="flex flex-col justify-center items-center mx-1">
-      <h1 className="font-bold my-2">Today</h1>
-      <SunLineIcon className="my-2" />
-      <ul className="flex flex-col flex-nowrap">
-        <li className="flex justify-between">
-          <div className="text-xs font-semibold">name:</div>
-          <div className="text-xs font-normal">num</div>
+    <div className="flex flex-col justify-center w-20 items-center mx-0.5 p-1 border-cyan-600 border-2">
+      <h1 className="font-bold">Today</h1>
+      <SunLineIcon className="my-1" />
+      <ul className="flex flex-col flex-nowrap w-full">
+        <li className="flex justify-between items-center my-1">
+          <SunriseAlt size="16px" />
+          <div className="text-xs font-normal">6:15</div>
         </li>
-        <li className="flex justify-between">
-          <div className="text-xs font-semibold">name:</div>
-          <div className="text-xs font-normal">num</div>
+
+        <li className="flex justify-between items-center my-1">
+          <Sunset size="16px" className="mr-3" />
+          <div className="text-xs font-normal">19:20</div>
         </li>
-        <li className="flex justify-between">
-          <div className="text-xs font-semibold">name:</div>
-          <div className="text-xs font-normal">num</div>
+
+        <li className="flex justify-between items-center my-1">
+          <TemperatureUp size="16px" className="mr-3" />
+          <div className="text-xs font-normal">40°</div>
         </li>
-        <li className="flex justify-between">
-          <div className="text-xs font-semibold">name:</div>
-          <div className="text-xs font-normal">num</div>
+
+        <li className="flex justify-between items-center my-1">
+          <TemperatureDown size="16px" className="mr-3" />
+          <div className="text-xs font-normal">25°</div>
         </li>
       </ul>
     </div>
--- src/components/DetailComponent.jsx
+++ src/components/DetailComponent.jsx
@@ -2,7 +2,7 @@ import SunLineIcon from "remixicon-react/SunLineIcon";
 
 function DetailComponent(props) {
   return (
-    <div className="flex flex-col items-center justify-center my-2 ">
+    <div className="flex flex-col items-center justify-center my-2">
       <h1 className="font-semibold text-base text-center">{props.name}</h1>
       <SunLineIcon className="my-1" />
       <p>{props.number}</p>
