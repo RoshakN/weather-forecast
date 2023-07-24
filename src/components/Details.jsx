454a540cacd410da02bd03200b290e50be2cfb8b
RoshakN <msnooraniz@gmail.com>
2023-07-24 22:16:03 +0330

    Fix details values

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -4,12 +4,18 @@ function Details(props) {
   return (
     <div className="flex flex-row flex-wrap items-center justify-center w-4/5 mb-6 bg-blue-600 bg-opacity-30 rounded-md">
       <div className="flex flex-row flex-wrap justify-around items-center w-1/2">
-        <DetailComponent name="Humidity" val="10" />
-        <DetailComponent name="UV Index" val="20%" />
+        <DetailComponent name="Humidity" val={props.weather.humidity} />
+        <DetailComponent name="UV Index" val={props.weather.current_uv_index} />
       </div>
       <div className="flex flex-row flex-wrap justify-around items-center w-1/2">
-        <DetailComponent name="Visibilty" val="30%" />
-        <DetailComponent name="Chance of Rain" val="40%" />
+        <DetailComponent
+          name="Visibilty"
+          val={props.weather.current_visibility}
+        />
+        <DetailComponent
+          name="Chance of Rain"
+          val={props.weather.rain_probability}
+        />
       </div>
     </div>
   );
