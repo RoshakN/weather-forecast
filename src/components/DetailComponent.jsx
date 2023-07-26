8e063ed83754e96850b6ff43738c9a86706c40f8
RoshakN <msnooraniz@gmail.com>
2023-07-26 19:27:15 +0330

    Fix details icons

--- src/components/DetailComponent.jsx
+++ src/components/DetailComponent.jsx
@@ -1,10 +1,25 @@
-import { CloudSun } from "react-flaticons";
+import { Sun, Eye, Humidity, Raindrops } from "react-flaticons";
 
 function DetailComponent(props) {
+  // const getDetailIcon = ({ icon }) => {
+  //   const DetailIcon = icon;
+  //   return DetailIcon;
+  // };
+  // const IconName = getDetailIcon(props.icon);
+  let DetailIcon =
+    props.icon === "Humidity" ? (
+      <Humidity className="my-2" />
+    ) : props.icon === "Visibility" ? (
+      <Eye className="my-2" />
+    ) : props.icon === "Rain" ? (
+      <Raindrops className="my-2" />
+    ) : (
+      <Sun className="my-2" />
+    );
   return (
     <div className="flex flex-col items-center justify-center my-2">
       <h1 className="font-semibold text-base text-center">{props.name}</h1>
-      <CloudSun className="my-2" />
+      <>{DetailIcon}</>
       <p className="font-light text-sm text-center">
         {props.val}
         {props.sign}
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -8,23 +8,31 @@ function Details() {
   return (
     <div className="flex flex-row flex-wrap items-start justify-center w-4/5 mb-6 p-2 bg-blue-600 bg-opacity-30 rounded-md">
       <div className="flex flex-row flex-wrap justify-around items-center w-1/2">
-        <DetailComponent name="Humidity" val={weather.humidity} sign="%" />
+        <DetailComponent
+          name="Humidity"
+          val={weather.humidity}
+          sign="%"
+          icon="Humidity"
+        />
         <DetailComponent
           name="UV Index"
           val={weather.current_uv_index}
           sign=""
+          icon="UV Index"
         />
       </div>
       <div className="flex flex-row flex-wrap justify-around items-center w-1/2">
         <DetailComponent
-          name="Visibilty"
+          name="Visibility"
           val={weather.current_visibility}
           sign="m"
+          icon="Visibility"
         />
         <DetailComponent
           name="Chance of Rain"
           val={weather.rain_probability}
           sign="%"
+          icon="Rain"
         />
       </div>
     </div>
